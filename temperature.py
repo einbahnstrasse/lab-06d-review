45deb801a1d7f78e260520e17e2759512a9db085
Louis Goldford <LGoldford@citytech.cuny.edu>
2023-10-05 02:24:05 +0200

    +temperature html and python versions

---
+++ temperature.py
@@ -0,0 +1,3 @@
+tempincelsius = input("Please enter a temperature in Celsius: ")
+tempinfahrenheit = float(tempincelsius) * 1.8 + 32
+print("The temperature is " + str(tempinfahrenheit) + " degrees F")
